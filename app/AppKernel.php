083648ce0a2755c7587496f11aeab443afc37e03
Romain Monceau <romain@akeneo.com>
2015-02-05 10:28:16 +0100

    Add CustomEntityBundle dependency

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -99,9 +99,10 @@ class AppKernel extends OroKernel
 
         $bundles = array_merge($bundles, $enterpriseBundles);
 
+        $bundles[] = new Pim\Bundle\CustomEntityBundle\PimCustomEntityBundle();
+
         $bundles[] = new Idrolab\Bundle\InstallerBundle\IdrolabInstallerBundle();
-        
-        
+
         return $bundles;
     }
 
